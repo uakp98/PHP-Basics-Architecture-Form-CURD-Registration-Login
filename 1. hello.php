a8cb41c7f2aa8cf56209a22d110e2a0245a136bb
Apurba Kumar Pramanik <59066595+uakp98@users.noreply.github.com>
2021-06-02 16:55:29 +0530

    Add files via upload

---
+++ 1. hello.php
@@ -0,0 +1,19 @@
+<html>
+<head></head>
+<body>
+	<?php
+		echo "<h1>Hello World!<hr>";
+		// this is used to single line comment
+		$car="volvo";
+		#this $ is used to declare a variable
+		/*$x=5;
+		
+		echo "My car name is: ".$car."<br>";*/
+		echo "My car name is: $car";
+		
+		
+	
+	?>
+	<p>
+</body>
+</html>
